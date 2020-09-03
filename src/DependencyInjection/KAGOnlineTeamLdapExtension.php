80a67cee73ae9051d26ccb0015548fba4d3d4c8b
indecim <janjo457boy@gmail.com>
2020-09-03 16:21:11 +0200

    Replace the xml file loader

--- src/DependencyInjection/KAGOnlineTeamLdapExtension.php
+++ src/DependencyInjection/KAGOnlineTeamLdapExtension.php
@@ -5,7 +5,7 @@ namespace KAGOnlineTeam\LdapBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 /**
  * @author Jan Flaßkamp
@@ -14,8 +14,7 @@ final class KAGOnlineTeamLdapExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
-        $loader->load('ldap_services.xml');
+        $loader = new PhpFileLoader($container, new FileLocator(\dirname(__DIR__).'/Resources/config'));
 
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
