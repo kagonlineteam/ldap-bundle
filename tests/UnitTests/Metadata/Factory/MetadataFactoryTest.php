e026bc35438df8c3594a62b040a46099cf4c823d
indecim <janjo457boy@gmail.com>
2020-09-16 15:07:42 +0200

    Fix tests

--- tests/UnitTests/Metadata/Factory/MetadataFactoryTest.php
+++ tests/UnitTests/Metadata/Factory/MetadataFactoryTest.php
@@ -4,6 +4,7 @@ namespace KAGOnlineTeam\LdapBundle\Tests\UnitTests\Metadata\Factory;
 
 use InvalidArgumentException;
 use KAGOnlineTeam\LdapBundle\Exception\NoMetadataException;
+use KAGOnlineTeam\LdapBundle\Metadata\ClassMetadataInterface;
 use KAGOnlineTeam\LdapBundle\Metadata\Extractor\ExtractorInterface;
 use KAGOnlineTeam\LdapBundle\Metadata\Factory\MetadataFactory;
 use KAGOnlineTeam\LdapBundle\Tests\Fixtures\DummyUser;
@@ -15,11 +16,11 @@ class MetadataFactoryTest extends TestCase
     public function testFound()
     {
         $extractor1 = $this->prophesize(ExtractorInterface::class);
-        $extractor1->extractFor(Argument::which('getClass', DummyUser::class))->willThrow(new NoMetadataException())->shouldBeCalled();
+        $extractor1->extractFor(Argument::type(ClassMetadataInterface::class))->willThrow(new NoMetadataException())->shouldBeCalled();
 
         $extractor2 = $this->prophesize(ExtractorInterface::class);
-        $extractor2->extractFor(Argument::which('getClass', DummyUser::class))->will(function ($metadata) {
-            $metadata->setObjectClasses(['inetOrgPerson']);
+        $extractor2->extractFor(Argument::type(ClassMetadataInterface::class))->will(function ($args) {
+            $args[0]->setObjectClasses(['inetOrgPerson']);
         })->shouldBeCalled();
 
         $factory = new MetadataFactory([$extractor1->reveal(), $extractor2->reveal()]);
